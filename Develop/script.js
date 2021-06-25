21b0ab768223b38d06ec94f18bccfc365440a462
Kevin Choi <kevchoi1028@gmail.com>
2021-06-24 21:16:31 -0700

    uploaded a screenshot of the page to be used in README file. READ ME file has been modified. Fully modified the JS file.

--- Develop/script.js
+++ Develop/script.js
@@ -1,70 +1,86 @@
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-
-  function generatePassword() {
-    var choi = "Hello12345";
-    return choi;
-  };
-
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Creates prompts and generate randomPassword based on user's input
+function generatePassword() {  
+  // Creating variables for the list of special, numeric characters and lowercase, uppercase letters in strings
+  var specialCharacter = "!#$%&()*+,-:;<=>?@[]^_{|}~";
+  var numericCharacter = "0123456789";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+  // Creating an empty string to act as a placeholder, such that the characters that passed the criteria can be stored here
+  var criteriaPassedCharacters = "";
+  
+  // Asks user how many characters to create for the password
+  var userNumChar = prompt("How many characters would you like your password to contain?");
 
-// First prompt 
-var userNumChar = prompt("How many characters would you like your password to contain?");
-console.log(userNumChar);
+  // If user input is less than 8 and greater than 128, then alert people with the message saying the input must be in between 8 and 128, inclusive
+  if (userNumChar < 8 && userNumChar > 128) {
+    alert("The number of characters must be greater than or eaual to 8 and less than or equal to 128.");
+  } 
+    // If user choses the number in between 8 and 128, inclusive, then ask 4 different criteria (yes/no) for the password
+    // concat() allows to join multiple strings into one string
+    else {
+    var specialCharYes = confirm("Would you like to include special characters?");
+    if (specialCharYes){
+      specialCharYes = true;
+      criteriaPassedCharacters = criteriaPassedCharacters.concat(specialCharacter);
+    } else {
+      specialCharYes = false;
+    }
+    var numericCharYes = confirm("Would you like to include numeric characters?");
+    if (numericCharYes){
+      numericCharYes = true;
+      criteriaPassedCharacters = criteriaPassedCharacters.concat(numericCharacter);
+    } else {
+      numericCharYes = false;
+    }
+    var lowercaseCharYes = confirm("Would you like to include lowercase letter?");
+    if (lowercaseCharYes){
+      lowercaseCharYes = true;
+      criteriaPassedCharacters = criteriaPassedCharacters.concat(lowerCase);
+    } else {
+      lowercaseCharYes = false;
+    }
+    var uppercaseCharYes = confirm("Would you like to include uppercase letter?");
+    if (uppercaseCharYes){
+      uppercaseCharYes = true;
+      criteriaPassedCharacters = criteriaPassedCharacters.concat(upperCase);
+    } else {
+      uppercaseCharYes = false;
+    }
+  }
 
-if (userNumChar < 8) {
-  alert("The number of characters must be at least 8.");
-} else if (userNumChar > 128) {
-  alert("The number of characters must not exceed 128.");
-} else {  
-  var criteriaCheck = ["special characters", "numeric characters", "lowercase letters", "uppercase letters"];
-  for (i = 0; i < criteriaCheck.length; i++) {
-    if (criteriaCheck[i]) {
-      var criteriaCheckMessage = confirm("Would you like to add " + criteriaCheck[i] + "?");
-      if (criteriaCheck[i]) {
-        criteriaCheck[i] = true;
-      } else {
-        criteriaCheck[i] = false;
-      }
-    }  
-  };  
+  // Message notifies users that at least one criteria must be selected to proceed
+  if (specialCharYes === false && numericCharYes === false &&
+      lowercaseCharYes === false && uppercaseCharYes === false) {
+    
+    alert("There must be at least one character type selected to proceed.");
+  }
+
+  // Creating an empty string to act as a placeholder, such that the ramdomly pulled characters from the cirteriaPassedCharacters can be stored here
+  var randomPassword = "";
+
+  // Going through the for loop and it generates a random character for 'userNumCar' times and store the characters inside the empty string, randomPassWord
+  for (let i = 0; i < parseInt(userNumChar); i++) {
+    randomPick = Math.floor(Math.random()*criteriaPassedCharacters.length);
+    randomPassword = randomPassword.concat(criteriaPassedCharacters.charAt(randomPick)); 
+  }
+
+  // Return the randomPassword based on user's input/request
+  return randomPassword;
+
 };
 
-// x = criteriaCheck[4].value;
-// console.log(x);
-
-if (criteriaCheck[0] === false && criteriaCheck[1] === false &&
-    criteriaCheck[2] === false && criteriaCheck[3] === false) {
-  
-  alert("There must be at least one character type selected to proceed.");
-}
-
-console.log(criteriaCheck.value);
-
-
-var specialCharacter = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-console.log(specialCharacter.substr(Math.floor(specialCharacter.length*Math.random()),1));
-
-var numericCharacter = "0123456789";
-console.log(numericCharacter.substr(Math.floor(numericCharacter.length*Math.random()),1));
-
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-console.log(lowerCase.substr(Math.floor(lowerCase.length*Math.random()),1));
-
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-console.log(upperCase.substr(Math.floor(upperCase.length*Math.random()),1));
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# password-generator
+# Unit 03 HW: password-generator
+
+## Description
+This project allows an user to generate a random password based on their choice of criteria, such as the number of characters, special characters, numeric characters, lowercase characters, and uppercase characters. When user click a 'generate password' button, they are prompted with questions about slecting their criteria for thier password. Based on their criteria, it will generate random password and display on the window. The number of the password must be at least 8 characters but must not exceed 128 characters.
+
+## Tasks Assigned
+```
+As an employee with access to sensitive data, he or she would like to randomly generate a password that meets certain criteria. This pass generator should be able to provide strong password under the criteria and perform high security.
+```
+- 
+
+## Tasks Completed
+
+```
+As a web developer,
+The following tasks were completed to meet the client's criteria:
+```
+- [x] When button is clicked, an employee is presented with a series of prompts for password criteria
+- [x] An employee is able to select their preferred criteria
+- [x] An employee is prompted with the length of the password and he or she must provide any number in between 8 and 128, inclusive. Otherwise, they will get an alert message
+- [x] An employee is prompted to select the character criteria(types) and it is systemed so that at least one criteria is needed to proceed to generate the password
+- [x] When an user select all criteria, the inputs are stored and execute the code to generate random password that meets all criteria
+- [x] When the password is generated, it successfuly displays in the browser window.
+
+## Screenshot of the Website
+
+![screenshot of index.html](./assets/screenshot-of-full-page.png)
+
+## Links
+
+Deployed Website Link: [https://rhwlffk1028.github.io/code-refactoring-for-client/](https://rhwlffk1028.github.io/code-refactoring-for-client/)
+
+Code Repository Link: [https://github.com/rhwlffk1028/code-refactoring-for-client/](https://github.com/rhwlffk1028/code-refactoring-for-client/)
+
+> **Note**: Use the first link above to access the website. By clicking the deployed webiste link, you will be directed to the web browser and it will be viewable.<br>
+> **Note**: Use the second link above to access the repository where the html, css, and image files are live.
+
+## Copyright
+---
+Copyright (c) [2021] [Jun Choi] <br>
+Confidential and Proprietary. All Rights Reserved.
